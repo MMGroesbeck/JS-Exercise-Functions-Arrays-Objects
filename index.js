e1e5d357193826222a48a3b147f432f20c818e7a
M Groesbeck <m.groesbeck@gmail.com>
2020-01-27 13:05:43 -0800

    Stretch goals completed

--- index.js
+++ index.js
@@ -307,8 +307,15 @@ const argTimesTwo = (num) => num * 2; // code here!
  *         (1) causes the odometer in the object to be increased by the distance,
  *         (2) returns the updated value of the `odometer`.
 */
-function carMaker(/* code here */) {
-  /* code here */
+function carMaker(miles) {
+  let odo = {
+    odometer: miles,
+    drive: function(distance){
+      odo.odometer += distance;
+      return odo.odometer;
+    }
+  }
+  return odo;
 }
 
 /// ////// END OF CHALLENGE /////////
